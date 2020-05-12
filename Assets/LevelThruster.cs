6321e9fffe9ffd08dd438085e23d68f254c751f2
Maximilian Maroe <drakfyre@gmail.com>
2020-05-11 20:47:56 -0700

    Basic level movement

---
+++ Assets/LevelThruster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelThruster : MonoBehaviour
+{
+    public float force = 10.0f;
+    public ForceMode forceMode;
+    public string buttonName = "Fire1";
+
+    [Header("Required Scene References")]
+    public Transform playerTransform;
+
+    [Header("Required Component References")]
+    public new Rigidbody rigidbody;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton(buttonName))
+        {
+            //We apply the force to the player's downward transform as we are thrusting the level
+            rigidbody.AddForce(-playerTransform.up*force,forceMode);
+        }
+    }
+
+    void OnValidate()
+    {
+        if(rigidbody == null)
+        {
+            rigidbody = GetComponentInChildren<Rigidbody>();
+        }
+    }
+}
---
+++ Assets/RotateLeftAndRight.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLeftAndRight : MonoBehaviour
+{
+    public float rotationSpeed = 60.0f;
+    public string axisName = "Horizontal";
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0,Input.GetAxis(axisName)*rotationSpeed*Time.deltaTime);
+    }
+}
