43e9e8059dee50cf978f387368feccdde938ec65
Maximilian Maroe <drakfyre@gmail.com>
2020-05-12 15:19:51 -0700

    Dumping in save system from discgame

---
+++ Assets/Scripts/SaveSystem/LevelManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;             
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public string levelDefinitionFileName;
+
+    LevelDefinition levelDefinition = new LevelDefinition();
+
+    [System.Serializable]
+    public class ObjectDefinition
+    {
+        public LevelDefinition.ObjectType type;
+        public GameObject prefab;
+    }
+
+    public ObjectDefinition[] objectDefinitions;
+
+    public Dictionary<LevelDefinition.ObjectType,GameObject> objectDefinitionDictionary = new Dictionary<LevelDefinition.ObjectType,GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(ObjectDefinition def in objectDefinitions)
+        {
+            objectDefinitionDictionary.Add(def.type,def.prefab);
+        }
+        if(!string.IsNullOrEmpty(levelDefinitionFileName))
+        {
+            string filePath = Path.Combine(Application.streamingAssetsPath, levelDefinitionFileName + ".json");
+            if(File.Exists(filePath))
+            {
+                levelDefinition = JsonUtility.FromJson<LevelDefinition>(File.ReadAllText(filePath));
+                foreach(LevelDefinition.ObjectData data in levelDefinition.objectData)
+                {
+                    GameObject obj = Instantiate(objectDefinitionDictionary[data.type],data.position,data.rotation);
+                    obj.transform.localScale = data.scale;
+                    if(data.type == LevelDefinition.ObjectType.PullNode || data.type == LevelDefinition.ObjectType.PushNode)
+                    {
+                        //obj.GetComponent<ForceBall>().forceEditable.value = data.magnitude;
+                    }
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnApplicationQuit()
+    {
+        levelDefinition.objectData.Clear();
+        foreach(SaveMe saveable in SaveMe.saveables)
+        {
+            LevelDefinition.ObjectData objectData = new LevelDefinition.ObjectData();
+            objectData.position = saveable.transform.position;
+            objectData.rotation = saveable.transform.rotation;
+            objectData.scale = saveable.transform.localScale;
+            objectData.type = saveable.type;
+
+            if(saveable.type == LevelDefinition.ObjectType.Goal)
+            {
+                //objectData.magnitude = saveable.GetComponent<ForceBall>().forceEditable.value;
+            }
+            else if(saveable.type == LevelDefinition.ObjectType.PullNode)
+            {
+                //objectData.magnitude = saveable.GetComponent<ForceBall>().forceEditable.value;
+            }
+            else if(saveable.type == LevelDefinition.ObjectType.PushNode)
+            {
+                //objectData.magnitude = saveable.GetComponent<ForceBall>().forceEditable.value;
+            }
+
+            levelDefinition.objectData.Add(objectData);
+        }
+
+        if(string.IsNullOrEmpty(levelDefinitionFileName))
+        {
+            levelDefinitionFileName = System.Guid.NewGuid().ToString();
+        }
+        string filePath = Path.Combine(Application.streamingAssetsPath, levelDefinitionFileName + ".json");
+
+        File.WriteAllText(filePath,JsonUtility.ToJson(levelDefinition));
+    }
+}
---
+++ Assets/Scripts/SaveSystem/SaveMe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveMe : MonoBehaviour
+{
+    public static List<SaveMe> saveables = new List<SaveMe>();
+    public LevelDefinition.ObjectType type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        saveables.Add(this);
+    }
+
+    void OnDestroy()
+    {
+        saveables.Remove(this);
+    }
+}
