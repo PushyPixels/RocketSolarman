0217376aadb1318326a044c67a7916713a3ec341
Maximilian Maroe <drakfyre@gmail.com>
2020-05-13 00:01:53 -0700

    Basic enemy placement (1 enemy, no spawners, no serialization)

---
+++ Assets/Scripts/PhysicsFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsFollow : MonoBehaviour
+{
+    public Rigidbody target;
+
+    public new Rigidbody rigidbody;
+
+    // Update is called once per physics update
+    void FixedUpdate()
+    {
+        rigidbody.velocity = target.velocity;
+    }
+
+    void OnValidate()
+    {
+        if(rigidbody == null)
+        {
+            rigidbody = GetComponentInChildren<Rigidbody>();
+        }
+    }
+}
