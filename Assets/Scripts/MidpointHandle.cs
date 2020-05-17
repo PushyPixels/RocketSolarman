ae276eb3cc467d599c78f61434c944a9bbcedee4
Maximilian Maroe <drakfyre@gmail.com>
2020-05-17 03:06:13 -0700

    Polytool in progress

---
+++ Assets/Scripts/MidpointHandle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MidpointHandle : MonoBehaviour
+{
+    public MeshFilter meshFilter;
+    // Midpoint needs to have two associated triangles
+    public 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
