080e704ee2fa288f5bee8863ea65fe48e3302ee5
Maximilian Maroe <drakfyre@gmail.com>
2020-05-18 20:45:10 -0700

    Rotating bullets

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float rotationSpeed = 60.0f;
+    public Vector3 axis = Vector3.forward;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(axis,rotationSpeed*Time.deltaTime);
+    }
+}
