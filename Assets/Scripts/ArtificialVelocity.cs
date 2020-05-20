9ec0e6653ee26d8f44524663e19ccc3c2053ff4a
Maximilian Maroe <drakfyre@gmail.com>
2020-05-19 17:28:29 -0700

    Player bullets (No collision but they inherit velocity)

---
+++ Assets/Scripts/ArtificialVelocity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtificialVelocity : MonoBehaviour
+{
+    public Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TODO: Add rigidbody to game manager
+        velocity = -GameManager.instance.levelTransform.GetComponent<Rigidbody>().velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += velocity*Time.deltaTime;
+    }
+}
